fa0cdde7d8125659e2ddf18e9654d133002a33a9
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-07-27 21:54:48 +0200

    Improving Exceptions

--- src/main/java/org/manuel/teambuilting/core/exceptions/ExceptionMessage.java
+++ src/main/java/org/manuel/teambuilting/core/exceptions/ExceptionMessage.java
@@ -1,14 +1,15 @@
 package org.manuel.teambuilting.core.exceptions;
 
-import org.springframework.http.HttpStatus;
-
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 /**
  * @author manuel.doncel.martos
  * @since 13-1-2017
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @AllArgsConstructor
 public class ExceptionMessage {
