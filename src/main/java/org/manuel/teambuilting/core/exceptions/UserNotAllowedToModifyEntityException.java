0bb4c5c52d102434c3a3a35b751c5a2297079b1d
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-08-01 19:06:59 +0200

    Improving Exceptions

--- src/main/java/org/manuel/teambuilting/core/exceptions/UserNotAllowedToModifyEntityException.java
+++ src/main/java/org/manuel/teambuilting/core/exceptions/UserNotAllowedToModifyEntityException.java
@@ -1,8 +1,18 @@
 package org.manuel.teambuilting.core.exceptions;
 
+import lombok.AllArgsConstructor;
+
 /**
  * @author Manuel Doncel Martos
  * @since 17/04/2017.
  */
+@AllArgsConstructor
 public class UserNotAllowedToModifyEntityException extends RuntimeException {
+
+    private final String message;
+
+    public UserNotAllowedToModifyEntityException() {
+        this("");
+    }
+
 }
